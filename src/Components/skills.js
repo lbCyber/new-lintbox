61d68d57e03b136474a4d8cf67bccaead10b1c47
lbCyber <49038315+lbCyber@users.noreply.github.com>
2023-05-15 16:11:26 -0400

    Changed codepen blurbs

--- src/Components/skills.js
+++ src/Components/skills.js
@@ -27,17 +27,17 @@ const Skills = ({background}) => {
       "https://paulroc.ca"
     ],
     [
-      "An animation I made in CodePen!",
+      "An elaborate and layered animation I made, with over 100 unique frames but compact at 250kb",
       "./assets/codepen/cp-hr.jpg",
       "https://codepen.io/lbcyber/pen/BaLzpJY"
     ],
     [
-      "A small vector animation I made in CodePen!",
+      "An animated vector graphic image of an axolotl I made as my business' logo",
       "./assets/codepen/cp-axo.jpg",
       "https://codepen.io/lbcyber/pen/gOwaZzp"
     ],
     [
-      "The original Playstation startup screen I recreated in CodePen!",
+      "The original Playstation startup screen I recreated in CodePen, complete with old CRT scanline effects!",
       "./assets/codepen/cp-ps.jpg",
       "https://codepen.io/lbcyber/full/ExbmJmp"
     ],

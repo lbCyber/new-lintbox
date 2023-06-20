dc1d19beb50fe55e6af115b99106e0b47d532c9b
lbCyber <49038315+lbCyber@users.noreply.github.com>
2023-06-20 14:21:48 -0400

    Fixed style and reduced-motion issues

--- src/Components/skills.js
+++ src/Components/skills.js
@@ -31,7 +31,6 @@ const Skills = ({background, lightMode, reduceMotion}) => {
 
   const dummyBoxArray = useMemo(()=>[styleDummyBox, styleDummyBox, styleDummyBox, styleDummyBox], [styleDummyBox])
 
-  const [mouseReset, setMouseReset] = useState(false)
   const [mouseCoord, setMouseCoord] = useState({x: 0, y: 0});
   const [companyStyleBox, setCompanyStyleBox] = useState(dummyBoxArray);
   const [companyStyleShine, setCompanyStyleShine] = useState(0);
@@ -52,16 +51,12 @@ const Skills = ({background, lightMode, reduceMotion}) => {
       setShineReset(true);
       setCompanyStyleBox(dummyBoxArray);
       setCompanyStyleShine(0);
-      setMouseReset(true);
     };
     const mouseCap = (e) => {
-      if ((!isVisible || reduceMotion) && !mouseReset) {
+      if (!isVisible || reduceMotion) {
         doMouseReset();
       } else {
-        const newDummyBox = ["","","",""];
-        setCompanyStyleShine(isNaN(shineOp) ? 0 : shineOp);
         if (window.innerWidth > 1024) {
-          setMouseReset(false);
           setMouseCoord({x: e.clientX, y: e.clientY});
           setCenterX(logosRef.current.clientWidth / 2);
           setCenterY(window.innerHeight / 2);
@@ -69,73 +64,84 @@ const Skills = ({background, lightMode, reduceMotion}) => {
           setOffsetY(((mouseCoord.y - centerY) / centerY) * 20);
           setMidX(0.9 - (Math.abs(offsetX / 20) ?? 0));
           setMidY(0.9 - (Math.abs(offsetY / 20) ?? 0));
-          setShineOp(Math.max((((midX * .55) + (midY * .25)) / 2).toFixed(2), 0));
+          setShineOp(Math.max(((midX * .65) + (midY * .2)).toFixed(2), 0));
           setShineReset(false);
+          setCompanyStyleShine(isNaN(shineOp) ? 0 : shineOp);
+
+          const newDummyBox = ["","","",""];
+          const rotateX = (-.7 * offsetY).toFixed(2);
+          const rotateY = i => (((mouseCoord.x - (centerX * (.45 + (.35 * i)))) / centerX) * 10).toFixed(2);
+
           setCompanyStyleBox(
-          newDummyBox.map((i,k)=>`perspective(500px) rotateX(${(-.7 * offsetY).toFixed(2)}deg) rotateY(${(((mouseCoord.x - (centerX * (.45 + (.35 * k)))) / centerX) * 10).toFixed(2)}deg)`));
-        };
+          newDummyBox.map((i,k)=>`perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY(k)}deg)`));
+        } else {
+          doMouseReset();
+        }
       };
     };
     window.addEventListener('mousemove', mouseCap);
     return () => {
       window.removeEventListener('mousemove', mouseCap);
     }
-  }, [centerX, centerY, isVisible, mouseCoord, reduceMotion, midX, midY, offsetX, offsetY, shineOp, dummyBoxArray, mouseReset])
+  }, [centerX, centerY, isVisible, mouseCoord, reduceMotion, midX, midY, offsetX, offsetY, shineOp, dummyBoxArray])
+
+  const mapBackgroundPosition = i => {
+    const backgroundX = (300 + parseInt(mouseCoord.x * (.35 + (i * -.015)))) % 1000;
+    const backgroundY = (100 + parseInt(mouseCoord.y * .2 * (1 + i))) % 1000;
+    return `${backgroundX}px ${backgroundY}px`;
+  }
 
   return (
-    <>
-      <section
-        id="Skills"
-        className="skillsSection"
-        style={background}>
-        <div className="wrapper">
-          <div className="skillsContainer">
-            <div className="skillsDesc">
-              <h4>What I bring:</h4>
-              <p className="skillsText">
-                I'm a web developer and software engineer with +5 years experience developing fully-realized top-down web solutions for clients using Javascript, React, HTML/CSS, Ruby-on-Rails and Wordpress architectures. My goals are always to learn, improve, and employ modern accessibility standards to build a better web for all!
-              </p>
-              <div className="companiesContainer">
-                <h4>I've worked with:</h4>
-                <ul
-                  className="companiesList"
-                  ref={logosRef}>
-                  {companies.map((i, k) => {
-                    const imgURL = lightMode ? i[2] : i[1];
-                    return (
-                      <li
-                        className={`companyLogoItem
-                          ${shineReset ? " shineReset" : ""}`}
-                        key={k}
-                        style={{transform: companyStyleBox[k]}}>
-                        <div
-                          className="shine"
-                          style={{
-                            opacity: companyStyleShine,
-                            backgroundPosition: `${parseInt(offsetX - 25) * 5}px ${parseInt(offsetY + 700) * 3.25 * (k + 1)}px`}}></div>
-                        <img className="companyLogo"
-                          src={imgURL}
-                          alt={i[0]} />
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
-            </div>
-            <Codepen />
-          </div>
-        </div>
-        <div className="wrapper">
-          <div className="devIconsContainer">
-            <h4>My toolkit:</h4>
-            <div className="devIconsBox">
-              <DevIcons />
+    <section
+      id="Skills"
+      className="skillsSection"
+      style={background}>
+      <div className="wrapper">
+        <div className="skillsContainer">
+          <div className="skillsDesc">
+            <h4 className="fh4">What I bring:</h4>
+            <p className="skillsText">
+              I'm a web developer and software engineer with +5 years experience developing fully-realized top-down web solutions for clients using Javascript, React, HTML/CSS, Ruby-on-Rails and Wordpress architectures. My goals are always to learn, improve, and employ modern accessibility standards to build a better web for all!
+            </p>
+            <div className="companiesContainer">
+              <h4>I've worked with:</h4>
+              <ul
+                className="companiesList"
+                ref={logosRef}>
+                {companies.map((i, k) => {
+                  const imgURL = lightMode ? i[2] : i[1];
+                  return (
+                    <li
+                      className={`companyLogoItem
+                        ${shineReset ? " shineReset" : ""}`}
+                      key={k}
+                      style={{transform: companyStyleBox[k]}}>
+                      <div
+                        className="shine"
+                        style={{
+                          opacity: companyStyleShine,
+                          backgroundPosition: mapBackgroundPosition(k)
+                        }}></div>
+                      <img className="companyLogo"
+                        src={imgURL}
+                        alt={i[0]} />
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
           </div>
+          <Codepen />
         </div>
-      </section>
-    </>
-  )
-}
+        <div className="devIconsContainer">
+          <h4>My toolkit:</h4>
+          <div className="devIconsBox">
+            <DevIcons />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
